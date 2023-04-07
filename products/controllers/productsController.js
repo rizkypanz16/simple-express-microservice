3ce5fa36b9d43235c016a977861154af24b25b16
rizky panji <rizkypanji@example.com>
2023-04-07 09:26:18 +0700

    product module done

--- products/controllers/productsController.js
+++ products/controllers/productsController.js
@@ -3,7 +3,7 @@ var connection = config.connection;
 var bodyParser = require('body-parser');
 
 exports.getProducts = (req, res) => {
-    connection.query("SELECT product_id AS productId, product_name AS productName, product_description AS productDescription, product_price AS productPrice, product_image AS productImage, product_category AS productCategory, product_created_at AS productCreatedAt, product_updated_at AS productUpdatedAt FROM products", (error, results, fields) => { 
+    connection.query("SELECT product_id AS productId, product_name AS productName, product_description AS productDescription, product_quantity AS productQuantity, product_price AS productPrice, product_image AS productImage, product_category AS productCategory, product_created_at AS productCreatedAt, product_updated_at AS productUpdatedAt FROM products", (error, results, fields) => { 
         if (error) throw error;
         res.status(200);
         res.json(
@@ -16,7 +16,7 @@ exports.getProducts = (req, res) => {
 }
 
 exports.getProductsId = (req, res) => {
-    connection.query("SELECT product_id AS productId, product_name AS productName, product_description AS productDescription, product_price AS productPrice, product_image AS productImage, product_category AS productCategory, product_created_at AS productCreatedAt, product_updated_at AS productUpdatedAt FROM products WHERE product_id = '"+req.params.productId+"'", (error, results, fields) => { 
+    connection.query("SELECT product_id AS productId, product_name AS productName, product_description AS productDescription, product_quantity AS productQuantity, product_price AS productPrice, product_image AS productImage, product_category AS productCategory, product_created_at AS productCreatedAt, product_updated_at AS productUpdatedAt FROM products WHERE product_id = '"+req.params.productId+"'", (error, results, fields) => { 
         if (error) throw error;
         res.status(200);
         res.json(
@@ -28,8 +28,22 @@ exports.getProductsId = (req, res) => {
       });
 }
 
+exports.postProducts = (req, res) => {
+  var query = "INSERT INTO products (product_name, product_description, product_quantity, product_price, product_image, product_category) VALUES ('"+req.body.productName+"', '"+req.body.productDescription+"', "+req.body.productQuantity+", "+req.body.productPrice+", '"+req.body.productImage+"', "+req.body.productCategory+")";
+  connection.query(query, (error, results) => { 
+    if (error) throw error;
+    res.status(200);
+    res.json(
+      { 
+        status: "SUCCESS",
+        data: []
+      }
+    )
+  }); 
+}
+
 exports.putProducts = (req, res) => {
-  var query = "UPDATE products SET product_name='"+req.body.productName+"', product_description='"+req.body.productDescription+"', product_price="+req.body.productPrice+", product_image='"+req.body.productImage+"', product_category="+req.body.productCategory+" WHERE product_id="+req.params.productId+"";
+  var query = "UPDATE products SET product_name='"+req.body.productName+"', product_description='"+req.body.productDescription+"', product_quantity='"+req.body.productQuantity+"', product_price="+req.body.productPrice+", product_image='"+req.body.productImage+"', product_category="+req.body.productCategory+" WHERE product_id="+req.params.productId+"";
   connection.query(query, (error, results) => { 
     if (error) throw error;
     res.status(200);
--- products/routes/products.js
+++ products/routes/products.js
@@ -6,6 +6,8 @@ const productsController = require('./../controllers/productsController.js');
 
 router.get('/', productsController.getProducts);
 
+router.post('/', productsController.postProducts);
+
 router.get('/:productId', productsController.getProductsId);
 
 router.put('/:productId', productsController.putProducts);
