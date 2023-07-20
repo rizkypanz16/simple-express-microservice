9c0370630c0224aed55d1931c5ce3500ec0ae364
rizky panji <rizkypanji@example.com>
2023-07-20 10:01:26 +0700

    refactor(products): reorganize folder structure

--- products/index.js
+++ products/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const env = require('dotenv').config();
+const express = require("express");
+const env = require("dotenv").config();
 const PORT = 8002;
 const cors = require("cors");
 
@@ -7,32 +7,30 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 
-var products = require('././routes/products.js');
-var productCategories = require('././routes/productCategories.js');
+var products = require("./src/routes/products");
+var productCategories = require("./src/routes/productCategories");
 
 app.use(cors());
-app.use('/api/products', products);
-app.use('/api/product-categories', productCategories);
-app.get('/api', (req, res) => {
-    res.json(
-    {
-        "status": true,
-        "code": 200,
-        "message": "SUCCESS",
-        "api": "== products service =="
-    });
+app.use("/api/products", products);
+app.use("/api/product-categories", productCategories);
+app.get("/api", (req, res) => {
+  res.json({
+    status: true,
+    code: 200,
+    message: "SUCCESS",
+    api: "== products service ==",
+  });
 });
-app.get('/', (req, res) => {
-    res.json(
-    {
-        "status": true,
-        "code": 200,
-        "message": "SUCCESS",
-        "api": "== products service =="
-    });
+app.get("/", (req, res) => {
+  res.json({
+    status: true,
+    code: 200,
+    message: "SUCCESS",
+    api: "== products service ==",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log("== products service ==");
-    console.log(`server listening at http://localhost:8002`);
+  console.log("== products service ==");
+  console.log(`server listening at http://localhost:8002`);
 });
---
+++ products/src/controllers/productCategoriesController.js
@@ -0,0 +1,84 @@
+const config = require("../../config/connection");
+var connection = config.connection;
+var bodyParser = require("body-parser");
+
+exports.getProductCategories = (req, res) => {
+  connection.query(
+    "SELECT product_category_id AS productCategoryId, product_category_name AS productCategoryName, product_category_description AS productCategoryDescription, product_category_created_at AS productCategoryCreatedAt, product_category_updated_at AS productCategoryUpdatedAt FROM product_categories",
+    (error, results, fields) => {
+      if (error) throw error;
+      res.status(200);
+      res.json({
+        status: "OK",
+        data: results,
+      });
+    }
+  );
+};
+
+exports.getProductCategoriesId = (req, res) => {
+  connection.query(
+    "SELECT product_category_id AS productCategoryId, product_category_name AS productCategoryName, product_category_description AS productCategoryDescription, product_category_created_at AS productCategoryCreatedAt, product_category_updated_at AS productCategoryUpdatedAt FROM product_categories WHERE product_category_id = '" +
+      req.params.productCategoryId +
+      "'",
+    (error, results, fields) => {
+      if (error) throw error;
+      res.status(200);
+      res.json({
+        status: "OK",
+        data: results,
+      });
+    }
+  );
+};
+
+exports.postProductCategories = (req, res) => {
+  var query =
+    "INSERT INTO product_categories (product_category_name, product_category_description) VALUES ('" +
+    req.body.productCategoryName +
+    "', '" +
+    req.body.productCategoryDescription +
+    "' )";
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    res.status(200);
+    res.json({
+      status: "SUCCESS",
+      data: [],
+    });
+  });
+};
+
+exports.putProductCategories = (req, res) => {
+  var query =
+    "UPDATE product_categories SET product_category_name='" +
+    req.body.productCategoryName +
+    "', product_category_description='" +
+    req.body.productCategoryDescription +
+    "' WHERE product_category_id=" +
+    req.params.productCategoryId +
+    "";
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    res.status(200);
+    res.json({
+      status: "SUCCESS",
+      data: [],
+    });
+  });
+};
+
+exports.deleteProductCategories = (req, res) => {
+  var query =
+    "DELETE FROM product_categories WHERE product_category_id='" +
+    req.params.productCategoryId +
+    "'";
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    res.status(200);
+    res.json({
+      status: "SUCCESS",
+      data: [],
+    });
+  });
+};
