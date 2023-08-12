d8d97658b9e1efdc40ee34d1758948204d1f2ae3
rizky panji <rizkypanji@example.com>
2023-08-12 22:17:41 +0700

    fix(products): refactor method delete, put, post

--- products/src/controllers/productsController.js
+++ products/src/controllers/productsController.js
@@ -1,6 +1,5 @@
+require("body-parser");
 const connection = require("../../config/connection");
-// var connection = config.connection;
-var bodyParser = require("body-parser");
 
 exports.getProducts = (req, res) => {
   try {
@@ -95,23 +94,38 @@ exports.getProducts = (req, res) => {
   //   }
   // }
 };
-
-// exports.getProductsId = (req, res) => {
-//   connection.query(
-//     "SELECT products.product_id AS productId, products.product_name AS productName, products.product_description AS productDescription, products.product_quantity AS productQuantity, products.product_price AS productPrice, products.product_image AS productImage, products.product_category AS productCategoryId, product_categories.product_category_name AS productCategoryName, products.product_created_at AS productCreatedAt, products.product_updated_at AS productUpdatedAt FROM products INNER JOIN product_categories ON products.product_category = product_categories.product_category_id WHERE products.product_id = " +
-//       req.params.productId +
-//       "",
-//     (error, results, fields) => {
-//       if (error) throw error;
-//       res.status(200);
-//       res.json({
-//         status: "OK",
-//         data: results,
-//       });
-//     }
-//   );
-// };
-
+exports.getProductsId = (req, res) => {
+  let query =
+    "SELECT products.product_id AS productId, products.product_name AS productName, products.product_description AS productDescription, products.product_quantity AS productQuantity, products.product_price AS productPrice, products.product_image AS productImage, products.product_category AS productCategoryId, product_categories.product_category_name AS productCategoryName, products.product_created_at AS productCreatedAt, products.product_updated_at AS productUpdatedAt FROM products INNER JOIN product_categories ON products.product_category = product_categories.product_category_id WHERE products.product_id = " +
+    req.params.productId +
+    "";
+  try {
+    connection.query(query, (error, results) => {
+      if (!error) {
+        res.status(200);
+        res.json({
+          code: 200,
+          status: "Success",
+          data: results,
+        });
+      } else {
+        console.error(error);
+        res.status(500).json({
+          code: 500,
+          status: "Error",
+          message: "Internal Server Error",
+        });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      code: 500,
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+};
 exports.postProducts = (req, res) => {
   let query =
     "INSERT INTO products (product_name, product_description, product_quantity, product_price, product_image, product_category, product_created_at) VALUES ('" +
@@ -154,34 +168,50 @@ exports.postProducts = (req, res) => {
     });
   }
 };
-
-// exports.putProducts = (req, res) => {
-//   var query =
-//     "UPDATE products SET product_name='" +
-//     req.body.productName +
-//     "', product_description='" +
-//     req.body.productDescription +
-//     "', product_quantity='" +
-//     req.body.productQuantity +
-//     "', product_price=" +
-//     req.body.productPrice +
-//     ", product_image='" +
-//     req.body.productImage +
-//     "', product_category=" +
-//     req.body.productCategoryId +
-//     " WHERE product_id=" +
-//     req.params.productId +
-//     "";
-//   connection.query(query, (error, results) => {
-//     if (error) throw error;
-//     res.status(200);
-//     res.json({
-//       status: "SUCCESS",
-//       data: [],
-//     });
-//   });
-// };
-
+exports.putProducts = (req, res) => {
+  try {
+    let query =
+      "UPDATE products SET product_name='" +
+      req.body.productName +
+      "', product_description='" +
+      req.body.productDescription +
+      "', product_quantity='" +
+      req.body.productQuantity +
+      "', product_price=" +
+      req.body.productPrice +
+      ", product_image='" +
+      req.body.productImage +
+      "', product_category=" +
+      req.body.productCategoryId +
+      ", product_updated_at = NOW()" +
+      " WHERE product_id=" +
+      req.params.productId +
+      "";
+    connection.query(query, (error, results, fields) => {
+      if (!error) {
+        res.json({
+          code: 200,
+          status: "Success",
+          message: "Data Berhasil Di Update",
+        });
+      } else {
+        console.error(error);
+        res.status(500).json({
+          code: 500,
+          status: "Error",
+          message: "Internal Server Error",
+        });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      code: 500,
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+};
 exports.deleteProducts = (req, res) => {
   try {
     let query =
@@ -191,7 +221,7 @@ exports.deleteProducts = (req, res) => {
         res.json({
           code: 200,
           status: "Success",
-          message: "Data Berhasil Dihapus",
+          message: "Data Berhasil Di Hapus",
         });
       } else {
         console.error(error);
--- products/src/routes/products.js
+++ products/src/routes/products.js
@@ -1,17 +1,12 @@
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
 // import function from customerController
 const productsController = require("./../controllers/productsController.js");
 
 router.get("/", productsController.getProducts);
-
 router.post("/", productsController.postProducts);
-
-// router.get('/:productId', productsController.getProductsId);
-
-// router.put('/:productId', productsController.putProducts);
-
+router.get("/:productId", productsController.getProductsId);
+router.put("/:productId", productsController.putProducts);
 router.delete("/:productId", productsController.deleteProducts);
 
 //export this router to use in our index.js
